f5ac501db8ef14a8677a70ed444e5a8559cbe00f
Nobodycares-lo <140128868+Nobodycares-lo@users.noreply.github.com>
2024-08-11 21:18:30 -0600

    Added Gay Femboys Kissing

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,14 +1,12 @@
+/*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/service/chit/',
-
-    /* Bare server URL */ 
-    bare: 'https://muddy-rice-ba72.acsessremote.workers.dev/',
-    
+    prefix: '/service/',
+    bare: 'https://sunset-network-psi.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: '/uv.handler.js',
+    client: '/uv.client.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
 };
